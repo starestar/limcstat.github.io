612abe101a18ff659a5440577ca5b770665dc12f
GaTepSev <47729759+GaTepSev@users.noreply.github.com>
2021-04-02 16:58:46 +0900

    Update slideshowjs.js

--- js/slideshowjs.js
+++ js/slideshowjs.js
@@ -1,18 +1,25 @@
 var slideIndex = 0;
-
-function showSlides() {
+var timer = 3000;
+var timerId = null;
+function plusSlides(n) {
+  if(timerId != null) {
+    clearTimeout(timerId);
+    timerId = null;
+  }
+  showSlides(slideIndex += n - 1);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  if (slideIndex < 1) {slideIndex += slides.length}    
+  slides[slideIndex - 1].style.display = "block";  
+  timerId = setTimeout(showSlides, 3000, slideIndex);		
 }
